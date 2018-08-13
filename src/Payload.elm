ff99aa821ddd2c03d8ef98fb717ed38e5b6e44a9
ujiro99 <siro.cola@gmail.com>
2018-08-14 03:24:20 +0900

    ADD: ペイロードを含むメッセージ

---
+++ src/Payload.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
+
+
+-- Model
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- Message
+
+
+type Msg
+    = Increment Int
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick (Increment 2) ] [ text "+" ]
+        , text (toString model)
+        ]
+
+
+
+-- Update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment howMuch ->
+            ( model + howMuch, Cmd.none )
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- Main
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
