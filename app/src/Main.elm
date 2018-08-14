e95505b7eee1f702e53fad2d05bb8fa782552fcf
ujiro99 <siro.cola@gmail.com>
2018-08-14 22:02:47 +0900

    ADD: アプリケーションの開始

---
+++ app/src/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text, program)
+
+
+-- Model
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- Message
+
+
+type Msg
+    = NoOp
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
+
+
+
+-- Update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- Subscription
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- Main
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
